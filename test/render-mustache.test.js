f5104147ca3db05209000a3082bdecc5c23679ee
Jared Hanson <jaredhanson@gmail.com>
2013-02-22 12:16:58 -0800

    Add test case for engine.

--- test/render-mustache.test.js
+++ test/render-mustache.test.js
@@ -10,5 +10,19 @@ function(mustache) {
     
   });
   
+  describe("engine", function() {
+  
+    var engine = mustache();
+    
+    it('shoud return compiled template function', function() {
+      var template = engine('Hello {{name}}');
+      expect(template).to.be.a('function');
+      
+      var output = template({ name: 'World' });
+      expect(output).to.be.equal('Hello World');
+    });
+    
+  });
+  
   return { name: "test.render-mustache" }
 });
