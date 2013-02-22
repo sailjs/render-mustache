7af80538c9232f5bf0d929d4164076a44cb2b704
Jared Hanson <jaredhanson@gmail.com>
2013-02-22 12:35:47 -0800

    Add documentation regarding MIME types.

--- render-mustache.js
+++ render-mustache.js
@@ -15,7 +15,25 @@ function(Mustache) {
    *
    * Examples:
    *
-   *     render.engine('text/template', mustache());
+   *     render.engine('text/x-mustache-template', mustache());
+   *
+   * A Note on MIME Types:
+   *
+   * It has become common convention to include templates within HTML by
+   * enclosing them within script tags:
+   *
+   *     <script type=“text/template”>...</script>
+   *
+   * Recommended practice for Sail.js applications is to be more specific when
+   * indicating the MIME type, both as a way to communicate explicitly with
+   * other developers and as a means to use multiple template engines within an
+   * application.
+   *
+   * While no standard exists, the following list of MIME types are used to
+   * indicate Mustache templates in practice:
+   *
+   *  * text/x-mustache-template
+   *      [Announcing Handlebars.js](http://yehudakatz.com/2010/09/09/announcing-handlebars-js/)
    *
    * @return {Function}
    * @api public
